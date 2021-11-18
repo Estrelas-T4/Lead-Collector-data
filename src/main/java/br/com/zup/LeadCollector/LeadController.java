58808af459a16ec0969c67a45729a66e2366167d
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-18 14:44:01 -0300

    Implementar rota de acesso ao cadastro do Lead

--- src/main/java/br/com/zup/LeadCollector/LeadController.java
+++ src/main/java/br/com/zup/LeadCollector/LeadController.java
@@ -1,14 +1,19 @@
 package br.com.zup.LeadCollector;
 
+import br.com.zup.LeadCollector.lead.Lead;
 import br.com.zup.LeadCollector.lead.LeadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/leads")
 public class LeadController {
     @Autowired
     private LeadService leadService;
+
+    @PutMapping
+    public Lead cadastrarLead(@RequestBody Lead lead){
+        return leadService.salvarLead(lead);
+    }
 
 }
