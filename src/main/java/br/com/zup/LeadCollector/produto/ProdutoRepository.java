5a9fb2c7d0948e8ddc6dfb2e6101276ba0ac8911
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-18 14:23:01 -0300

    Criar interface de comunicacao com banco da dados para Produto

---
+++ src/main/java/br/com/zup/LeadCollector/produto/ProdutoRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.LeadCollector.produto;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+}
