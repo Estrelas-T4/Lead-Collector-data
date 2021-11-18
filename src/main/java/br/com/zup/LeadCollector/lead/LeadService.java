74e9b899259de64d80c81d34fefb5d6326d882f0
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-18 15:57:17 -0300

    Criar metodo para buscar produtos vinculados com o lead

--- src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
@@ -1,15 +1,39 @@
 package br.com.zup.LeadCollector.lead;
 
+import br.com.zup.LeadCollector.produto.Produto;
+import br.com.zup.LeadCollector.produto.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LeadService {
     @Autowired
     private LeadRepository leadRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
-    public Lead salvarLead(Lead lead){
+    public Lead salvarLead(Lead lead) {
+        List<Produto> produtos = buscarProdutos(lead.getProdutosDeInteresse());
+
+        lead.setProdutosDeInteresse(produtos);
         return leadRepository.save(lead);
+    }
+
+    private List<Produto> buscarProdutos(List<Produto> produtos) {
+        List<Produto> listaAtualizada = new ArrayList<>();
+
+        for (Produto produto : produtos) {
+            if (produtoRepository.existsByNome(produto.getNome())) {
+                listaAtualizada.add(produtoRepository.findByNome(produto.getNome()));
+            }else {
+                listaAtualizada.add(produto);
+            }
+        }
+
+        return listaAtualizada;
     }
 
 }
--- src/main/java/br/com/zup/LeadCollector/produto/ProdutoRepository.java
+++ src/main/java/br/com/zup/LeadCollector/produto/ProdutoRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
     boolean existsByNome(String nome);
+
+    Produto findByNome(String nome);
+
 }
