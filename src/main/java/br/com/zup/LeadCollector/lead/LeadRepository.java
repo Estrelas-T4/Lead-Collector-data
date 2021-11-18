cc6ee4567ba65a9e8cfb2cff9038ba6011d8627a
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-18 14:39:12 -0300

    Criar query de busca de Lead pelo nome do Produto

--- src/main/java/br/com/zup/LeadCollector/lead/LeadRepository.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadRepository.java
@@ -2,6 +2,9 @@ package br.com.zup.LeadCollector.lead;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LeadRepository extends CrudRepository<Lead, String> {
+    List<Lead> findAllByProdutosDeInteresseNome(String nomeDoProduto);
 
 }
