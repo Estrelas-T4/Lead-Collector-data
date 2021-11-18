ade4a872ed9413e6b79367741f26027976135abf
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-18 14:28:01 -0300

    Criar query de verificacao de produto cadastrado

--- src/main/java/br/com/zup/LeadCollector/produto/ProdutoRepository.java
+++ src/main/java/br/com/zup/LeadCollector/produto/ProdutoRepository.java
@@ -3,4 +3,5 @@ package br.com.zup.LeadCollector.produto;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+    boolean existsByNome(String nome);
 }
