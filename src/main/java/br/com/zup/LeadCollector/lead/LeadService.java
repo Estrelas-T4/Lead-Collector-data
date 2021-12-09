ada6538e5c0431714b7c24c28218a221039eb909
Vinícius <vinicius.raymundo@zup.com.br>
2021-12-09 10:53:40 -0300

    exemplo ruim de commit

--- src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
@@ -7,23 +7,39 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LeadService {
-    @Autowired
     private LeadRepository leadRepository;
-    @Autowired
     private ProdutoRepository produtoRepository;
+
+    @Autowired
+    public LeadService(LeadRepository leadRepository, ProdutoRepository produtoRepository) {
+        this.leadRepository = leadRepository;
+        this.produtoRepository = produtoRepository;
+    }
 
     public Lead salvarLead(Lead lead) {
         List<Produto> produtos = buscarProdutos(lead.getProdutosDeInteresse());
-        /*
-        O metodo tem um problema. Ele elimina os produtos já cadastrados do lead.
-        Isso não pode acontecer, faça a correção para que os produtos já cadastrado permaneçam e os novos sejam incluidos.
-         */
+
+        Optional<Lead> leadOptional = leadRepository.findById(lead.getEmail());
+        if (leadOptional.isPresent()){
+            Lead leadDoBanco = leadOptional.get();
+            //Atualiza a lista do banco de dados com os novos itens do produtos. APENA OS NOVOS.
+            for (Produto produto : produtos){
+                if(!leadDoBanco.getProdutosDeInteresse().contains(produto)){
+                    leadDoBanco.getProdutosDeInteresse().add(produto);
+                }
+            }
+
+            return leadRepository.save(leadDoBanco);
+        }
+
         lead.setProdutosDeInteresse(produtos);
         return leadRepository.save(lead);
     }
+
 
     private List<Produto> buscarProdutos(List<Produto> produtos) {
         List<Produto> listaAtualizada = new ArrayList<>();
@@ -42,5 +58,7 @@ public class LeadService {
     public List<Lead> buscarTodosPeloNomeProduto(String nomeProduto){
         return leadRepository.findAllByProdutosDeInteresseNome(nomeProduto);
     }
+
+
 
 }
--- src/main/java/br/com/zup/LeadCollector/produto/Produto.java
+++ src/main/java/br/com/zup/LeadCollector/produto/Produto.java
@@ -1,6 +1,7 @@
 package br.com.zup.LeadCollector.produto;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "produtos")
@@ -27,4 +28,16 @@ public class Produto {
         this.nome = nome;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return nome.equals(produto.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
 }
